bffe725ff7b4c47f6d13fc0d238acc7a4bdbc6e7
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2021-09-29 12:20:07 -0400

    Added logic to fetch gists from username search

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Box, Button, FormControl, Input, Stack } from '@mui/material';
 import './App.css';
 
-function App() {
+export default function App() {
+  // setting up state
+  const [favorites, setFavorites] = useState([]);
+  const [term, setTerm] = useState('');
+
+  // making a place for lifecycle behaviors
+  useEffect(() => {
+  }, []);
+
+  const getGists = (user: string) => {
+    return fetch("https:api.github.com/users/" + user + "/gists").then(data => data.json);
+  };
+
+  const onSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event?.preventDefault();
+    getGists(term);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack direction="row" spacing={2}>
+        {/* <FormControl variant="standard"> */}
+          <Input onChange={(e) => setTerm(e.target.value)} />
+          <Button variant="outlined" onClick={(e) => onSubmit(e)}>Search</Button>
+        {/* </FormControl> */}
+      </Stack>
+      {/* search results (populous or no results) or errors/result of bad requests go here */}
+      <Box></Box>
     </div>
   );
 }
-
-export default App;
